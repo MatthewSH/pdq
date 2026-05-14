ee1c20128c909a9d12b7a7a7cba20ea5bc74e07c
Matt Hatcher <matthatcher@pm.me>
2026-05-14 05:27:32 -0400

    add pdq

--- pdq.go
+++ pdq.go
@@ -1,7 +1,52 @@
 package pdq
 
-type PDQ struct{}
+import (
+	"fmt"
+	"image"
 
-func New() *PDQ {
-	return &PDQ{}
+	"github.com/MatthewSH/pdq/internal"
+)
+
+// Hash computes the PDQ perceptual hash of an image.
+// Returns the primary hash, quality score (0–100), and all 8 dihedral transform hashes.
+func Hash(img image.Image) (Result, error) {
+	resized := internal.ResizeBilinear(img, internal.ImageSize)
+
+	luma, err := internal.ToLuminance(resized, internal.ImageSize)
+	if err != nil {
+		return Result{}, fmt.Errorf("pdq: luminance conversion failed: %w", err)
+	}
+
+	filtered, err := internal.JaroszFilter(luma)
+	if err != nil {
+		return Result{}, fmt.Errorf("pdq: jarosz filter failed: %w", err)
+	}
+
+	quality, err := internal.ComputeQuality(filtered)
+	if err != nil {
+		return Result{}, fmt.Errorf("pdq: quality computation failed: %w", err)
+	}
+
+	dct := internal.DCT64To16(filtered)
+	dihedrals, err := internal.DihedralHashes(dct)
+	if err != nil {
+		return Result{}, fmt.Errorf("pdq: dihedral hashes failed: %w", err)
+	}
+
+	return Result{
+		Hash:      packHash(dihedrals[0]),
+		Quality:   quality,
+		Dihedrals: dihedrals,
+	}, nil
+}
+
+func packHash(hash [16]uint16) Hash256 {
+	var out Hash256
+
+	for i, v := range hash {
+		out[i*2] = byte(v)
+		out[i*2+1] = byte(v >> 8)
+	}
+
+	return out
 }
