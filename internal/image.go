118c3c319d8cb3e89bf9b67b5cfa7a9fc77ff69b
Matt Hatcher <matthatcher@pm.me>
2026-05-14 01:17:07 -0400

    adding luminance and bilinear helper functions

---
+++ internal/image.go
@@ -0,0 +1,42 @@
+package internal
+
+import (
+	"fmt"
+	"image"
+
+	"golang.org/x/image/draw"
+)
+
+func resizeBilinear(src image.Image, size int) image.RGBA64Image {
+	dst := image.NewRGBA(image.Rect(0, 0, size, size))
+	draw.BiLinear.Scale(dst, dst.Bounds(), src, src.Bounds(), draw.Src, nil)
+
+	return dst
+}
+
+func toLuminance(src image.RGBA, size int) ([]float32, error) {
+	bounds := src.Bounds()
+	if (bounds.Dx() != size) || (bounds.Dy() != size) {
+		return nil, fmt.Errorf("source image dimensions do not match expected: %dx%d vs %dx%d", bounds.Dx(), bounds.Dy(), size, size)
+	}
+
+	out := make([]float32, size*size)
+	pixels := src.Pix
+	stride := src.Stride
+
+	for y := 0; y < size; y++ {
+		rowOffset := y * stride
+		outOffset := y * size
+
+		for x := 0; x < size; x++ {
+			i := rowOffset + x*4
+			r := float32(pixels[i])
+			g := float32(pixels[i+1])
+			b := float32(pixels[i+2])
+
+			out[outOffset+x] = 0.299*r + 0.587*g + 0.114*b
+		}
+	}
+
+	return out, nil
+}
